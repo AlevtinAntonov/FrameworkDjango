340b44b8b59c61420b3294d66c477ca2cc499331
Alevtin <antonov3017@gmail.com>
2023-08-29 16:37:14 +0300

    Initial commit

---
+++ myapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world!")
+
+
+def about(request):
+    return HttpResponse("About us")
+
+Открываем файл urls.py в корневой директории проекта
+
